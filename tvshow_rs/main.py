54c30e8fa3de9275f2b044400005c5f6dd45ae13
hongkyun-bae <54279688+hongkyun-bae@users.noreply.github.com>
2026-02-27 14:52:01 +0900

    Add files via upload

--- tvshow_rs/main.py
+++ tvshow_rs/main.py
@@ -19,7 +19,7 @@ from methods.PM import excluded_watchTimes
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Run Experiment.")
-    parser.add_argument('--mode', type=int,default=-1, help='0 - Build Matrix, 1 - Preference Estimation, 2 - Evaluation')
+    parser.add_argument('--mode', type=int,default=-1, help='0 - Build Matrix, 1 - Evaluation')
     parser.add_argument('--method', nargs='?', default='Prop', help='ST RT PM PNM Prop Prop_P Prop_PM Prop_N')
     parser.add_argument('--CV', type=int, default=1, help='from 1~3 CV')
     parser.add_argument('--reg', type=float, default=0.01, help = '0~1')
@@ -60,37 +60,29 @@ if __name__ == '__main__':
     ## read data
     print('ReadData')
     wl_6m_df, epg_df = ReadData.readData()
-    wl_2m_df, wl_1m_df, epg2_df, epg1_df, programs2, users2, programs1, users1 = ReadData.Preprocessing(CV, wl_6m_df, epg_df, mode)
 
     if mode == 0:
-        print('Mode' + str(mode) + ': BuildMatrix')
-
-        start_time = time.time()
-        PrefMatrix, ConfMatrix = BuildMatrix.BuildMatrix(method, CV, epg2_df, wl_2m_df, programs2, users2)
-        print('PE time: {}'.format(time.time() - start_time))
-
-        PrefMatrix = PrefMatrix.divide(ConfMatrix, axis='index')
-        PrefMatrix = PrefMatrix.fillna(0.0)
-
-        ConvertFormat.BuildMM(PrefMatrix, ConfMatrix, factor, CV, method, reg)
-
-
-    elif mode == 1:
         print('Mode' + str(mode) + ': Episode-based estimation considering TV watching interval')
+
+        wl_2m_df, wl_1m_df, epg2_df, epg1_df, programs2, users2, programs1, users1 = ReadData.Preprocessing(CV, wl_6m_df, epg_df, mode)
 
         start_time = time.time()
         PrefMatrix, ConfMatrix = BuildDict.BuildDictMatrix_ver2(method, CV, epg2_df, wl_2m_df, programs2, users2)
+        
         print('Episode-based estimation time: {}'.format(time.time() - start_time))
 
         PrefMatrix = PrefMatrix.divide(ConfMatrix, axis='index')
         PrefMatrix = PrefMatrix.fillna(0.0)
         ConvertFormat.BuildMM(PrefMatrix, ConfMatrix, factor, CV, method, reg)
+        
         print("Complete: BuildMM")    
     
 
-    elif mode == 2:
+    elif mode == 1:
         print ('Mode' + str(mode) + ': Evaluate')
-        PrefMatrix, ConfMatrix = ReadData.readMatrix('ST', CV, D)
+        PrefMatrix, ConfMatrix = ReadData.readMatrix('Prop', CV, D)
+
+        wl_2m_df, wl_1m_df, epg2_df, epg1_df, programs2, users2, programs1, users1 = ReadData.Preprocessing(CV, wl_6m_df, epg_df, mode, PrefMatrix)
         
         predMatrix = 0
         if isConf == 0:
@@ -114,19 +106,19 @@ if __name__ == '__main__':
             time_factor_histogram = ReadData.readTimeFactorMatrix(CV)
 
             # For watched test set (pred)
-            Score, ndcgs, MRR = ShowTime.Experiment_ST(epg_df, wl_2m_df, Checked_TestSet, PrefMatrix, predMatrix, time_factor_histogram, log, logname, data_type=1)
+            Score, ndcgs, MRR = ShowTime.Experiment_ST(epg_df, wl_2m_df, Checked_TestSet, PrefMatrix, predMatrix, time_factor_histogram, log, 'log.txt', data_type=1)
             if D != 1:
                 # For non-watched test set (pred)
-                Score2, ndcgs2, MRR2 = ShowTime.Experiment_ST(epg_df, wl_2m_df, Unchecked_TestSet, PrefMatrix, predMatrix, time_factor_histogram, log, logname, data_type=0)
+                Score2, ndcgs2, MRR2 = ShowTime.Experiment_ST(epg_df, wl_2m_df, Unchecked_TestSet, PrefMatrix, predMatrix, time_factor_histogram, log, 'log.txt', data_type=0)
 
         print('Recommendation time :{}'.format(time.time() - start_time))
 
         if D == 2:
             Evaluate.Evaluate(Score2, ndcgs2, MRR2, Score2, ndcgs2, MRR2, method, CV, reg, factor, D)
-            print("Average of watchTime: " + str(np.average(excluded_watchTimes)))
+            # print("Average of watchTime: " + str(np.average(excluded_watchTimes)))
         else:
             Evaluate.Evaluate(Score, ndcgs, MRR, Score2, ndcgs2, MRR2, method, CV, reg, factor, D)
 
     else:
-        print ("Mode should be from 0 to 2.")
+        print ("Mode should be from 0 to 1.")
     #'''
